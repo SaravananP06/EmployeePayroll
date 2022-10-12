1666858ccb6089ff9f3184b418840eb6a7c7cac4
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-10-12 11:27:27 +0530

    [Saravanan]: Added Setting up the pay roll project

---
+++ src/main/java/com/bridgeLabz/employpayroll/EmploypayrollApplication.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.employpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploypayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploypayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgeLabz/employpayroll/EmploypayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.employpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploypayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
