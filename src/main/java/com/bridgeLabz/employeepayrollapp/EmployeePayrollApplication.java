e8d1408e95eab6553dfd926581e859443d4a0aa2
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-11-04 23:09:57 +0530

    [Saravanan]: Added lombok for logging

--- src/main/java/com/bridgeLabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgeLabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,14 +1,16 @@
 package com.bridgeLabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollApplication.class, args);
-		System.out.println("Welcome to Employee Payroll Application");
+		log.info("Employee payroll App Started");
 	}
 
 }
